b5ed2016ce109ddbe58a8b215ecc782ae9a335dc
sanayuj <sanayuj2255@gmail.com>
2023-12-01 23:52:50 +0530

    add logger

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
-const express=require("express")
-const app=express()
-require('dotenv').config();
-const dbConnection=require("./Config/dbConnection")
-const cors=require("cors")
-
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const dbConnection = require("./Config/dbConnection");
+const cors = require("cors");
+const logger = require("morgan");
 
 //DATABASE
-dbConnection.dbConnect()
+dbConnection.dbConnect();
 
 //CORS
-app.use(cors({
-    origin:process.env.ORIGIN,
-    method:["GET","POST"],
-    credentials:true
-}))
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    method: ["GET", "POST"],
+    credentials: true,
+  })
+);
+
+app.use(logger("dev"));
 
 app.listen(process.env.PORT, () => {
-    console.log(`Sever started at port ${process.env.PORT}`);
-  });
-  
+  console.log(`Sever started at port ${process.env.PORT}`);
+});
+
+module.exports = app;

726d544ab67d93af50a30ae3f826e5718432fb54
sanayuj <sanayuj2255@gmail.com>
2023-12-01 23:35:59 +0530

    setup cors and DB

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
 const express=require("express")
 const app=express()
+require('dotenv').config();
 const dbConnection=require("./Config/dbConnection")
+const cors=require("cors")
 
 
-
+//DATABASE
 dbConnection.dbConnect()
+
+//CORS
+app.use(cors({
+    origin:process.env.ORIGIN,
+    method:["GET","POST"],
+    credentials:true
+}))
+
+app.listen(process.env.PORT, () => {
+    console.log(`Sever started at port ${process.env.PORT}`);
+  });
+  
--- Config/dbConnection.js
+++ Config/dbConnection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+mongoose.set('strictQuery', true)
 module.exports = {
   dbConnect: async () => {
     try {

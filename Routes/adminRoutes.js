aa246b280cbd5ccecbd8ad700b2b9aab0674d0d3
sanayuj <sanayuj2255@gmail.com>
2023-12-06 13:52:00 +0530

    all user list and user block

--- Routes/adminRoutes.js
+++ Routes/adminRoutes.js
@@ -1,9 +1,15 @@
 const express=require("express")
 const router=express.Router()
 
-const {adminLogin}=require("../Controllers/adminController")
+const {adminLogin,listUser,disableUser}=require("../Controllers/adminController")
 
-
+//POST method
 router.post("/login",adminLogin)
+router.post("/disableuser",disableUser)
+
+
+
+//GET method
+router.get("/listuser",listUser)
 
 module.exports = router;

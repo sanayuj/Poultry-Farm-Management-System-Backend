eda31f705962ffd931edddb58f40567b916050fb
sanayuj <sanayuj2255@gmail.com>
2023-12-16 17:34:39 +0530

    admin viewed all user details

--- Routes/adminRoutes.js
+++ Routes/adminRoutes.js
@@ -2,7 +2,7 @@ const express=require("express")
 const router=express.Router()
 const adminAuth=require('../Middleware/adminAuth')
 const {adminLogin,listUser,disableUser}=require("../Controllers/adminController")
-const { showUserFarms } = require("../Controllers/userController")
+const { showUserFarms, getMedicineDetails, getFeedDetails, getMortalityDetails } = require("../Controllers/userController")
 
 //POST method
 router.post("/login",adminLogin)
@@ -13,5 +13,8 @@ router.post("/disableuser",adminAuth,disableUser)
 //GET method
 router.get("/listuser",adminAuth,listUser)
 router.get("/getuserFarm/:userId",adminAuth,showUserFarms)
+router.get("/getmedicinedetails/:userId",adminAuth,getMedicineDetails)
+router.get("/getfeeddetails/:userId",adminAuth,getFeedDetails)
+router.get("/getmortalitydetails/:userId",adminAuth,getMortalityDetails)
 
 module.exports = router;

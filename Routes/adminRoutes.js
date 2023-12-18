0dbc2af66042422054db47981c818607ef7aacb2
sanayuj <sanayuj2255@gmail.com>
2023-12-18 10:56:52 +0530

    admin chart showed with total users and farm

--- Routes/adminRoutes.js
+++ Routes/adminRoutes.js
@@ -3,7 +3,7 @@ const router=express.Router()
 const adminAuth=require('../Middleware/adminAuth')
 const {adminLogin,listUser,disableUser}=require("../Controllers/adminController")
 const { showUserFarms, getMedicineDetails, getFeedDetails, getMortalityDetails } = require("../Controllers/userController")
-
+const {chartDashboard}=require("../Controllers/dashboardController")
 //POST method
 router.post("/login",adminLogin)
 router.post("/disableuser",adminAuth,disableUser)
@@ -16,5 +16,5 @@ router.get("/getuserFarm/:userId",adminAuth,showUserFarms)
 router.get("/getmedicinedetails/:userId",adminAuth,getMedicineDetails)
 router.get("/getfeeddetails/:userId",adminAuth,getFeedDetails)
 router.get("/getmortalitydetails/:userId",adminAuth,getMortalityDetails)
-
+router.get("/admindashboard",adminAuth,chartDashboard)
 module.exports = router;
